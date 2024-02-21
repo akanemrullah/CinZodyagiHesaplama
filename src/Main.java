8524ee7f1ea359389387d17f8fbe4b4f00871093
Emrullah AKAN <akanemrullah@gmail.com>
2024-02-21 22:49:17 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // Gerekli degiskenler tanimlandi.
         int birthYear, n1;
         String zodiacSign = "";
         boolean isError = false;
 
+        // Scanner sinifina ait bir degiskeni projeye dahil ettik.
         Scanner input = new Scanner(System.in);
 
+        // Kullanicidan dogum yilini alip ilgili degiskene atadik.
         System.out.print("Doğum Yılınızı Giriniz : ");
         birthYear = input.nextInt();
 
+        // Dogum yilinin 12 ile modunu alip n1 adli degiskene atadik.
         n1 = birthYear % 12;
 
+        // n1 degiskeni ile beraber switch-case yapisi kullandik.
         switch (n1) {
             case 0:
                 zodiacSign = "Maymun";
@@ -55,6 +60,7 @@ public class Main {
                 isError = true;
                 break;
         }
+        // Herhangi bir hata yok ise kullaniciya burcu ile alakali geribildirim verildi.
         if (isError == false) {
             System.out.println("Çin Zodyağı Burcunuz : " + zodiacSign);
         }
